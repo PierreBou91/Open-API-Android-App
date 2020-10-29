0ab806542cb5e6c6038aef3c4fbaab1b2dd17154
Sicabou <pbouriat@audencia.com>
2020-10-29 13:03:10 +0100

    Navigation graph for Authentication and Auth fragments + layouts

--- app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthActivity.kt
@@ -1,10 +1,10 @@
-package com.codingwithmitch.openapi
+package com.codingwithmitch.openapi.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.codingwithmitch.openapi.R
 
 class AuthActivity : AppCompatActivity(){
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
